3c68f5373b66515a688f867ad2fae310e7a79463
Janko Radakovic <jankoradakovic@sda-lund-67-mbpro.lan>
2021-03-29 13:36:31 +0200

    Adds json data for cupcakes and wedding cakes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from "./Components/Footer";
 import Header from "./Components/Header"
 import ImageDivider from "./Components/ImageDivider"
+import Product from "./Components/Product";
 
 import "./styles/App.css";
 
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <div className="App">
       <Header />
+      <Product />
       <ImageDivider />
       <Footer />
     </div>
