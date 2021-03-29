95a86606cc9ef66bc2e40c690408afe949a18a42
Janko Radakovic <jankoradakovic@sda-lund-67-mbpro.lan>
2021-03-29 14:19:46 +0200

    Adds products dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import Footer from "./Components/Footer";
-import Header from "./Components/Header"
-import ImageDivider from "./Components/ImageDivider"
+import Header from "./Components/Header";
+import ImageDivider from "./Components/ImageDivider";
 import Product from "./Components/Product";
+import cupCakesJSON from "./assets/data/cupCakes.json";
 
 import "./styles/App.css";
 
 export default function App() {
+  const cupCakes = cupCakesJSON.map((item) => {
+    return (
+      <Product
+        key={item.id}
+        title={item.title}
+        price={item.price}
+        fileName={item.fileName}
+      />
+    );
+  });
+
   return (
     <div className="App">
       <Header />
-      <Product />
+      <div className="content-wrap">
+        <section className="content" id="cupcakes">
+          <h2>Cupcakes</h2>
+          {cupCakes}
+        </section>
+      </div>
       <ImageDivider />
       <Footer />
     </div>
--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,6 +1,16 @@
-export default function Product() {
+export default function Product({ title, price, fileName }) {
+  const cupCakeImages = require(`../assets/images/cupcakes/${fileName}`);
+  const imageURL = cupCakeImages.default;
+
   return (
-
-
-  )
+    <div className="grid">
+      <article className="product-card">
+        <img className="product-image" src={imageURL} alt="image" />
+        <div className="product-info">
+          <h3 className="title">{title}</h3>
+          <p className="price">{price}</p>
+        </div>
+      </article>
+    </div>
+  );
 }
